99725b77aad0ea6173e58aa961deb967f7bdd19b
jfroom <jeff@jfroom.com>
2017-03-08 20:26:32 -0800

    Hound tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
             email_prefix: "[#{ENV['APP_NAME'].upcase}::#{Rails.env}] ",
             sender_address: ENV['ACTION_MAILER_DEFAULT_FROM'],
             exception_recipients: [ENV['ACTION_MAILER_DEFAULT_TO']]
-          }
+         }
   Rails.application.config.middleware.use ExceptionNotification::Rack, email: opts
 end
